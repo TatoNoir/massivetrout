b2c8c975253c091872c4ec0040ed7d87feefb02a
Mariano <marianopainefil@gmail.com>
2026-08-04 11:56:01 -0300

    feat: configurar sitio para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://massivetroutflyfishing.com",
+  site: "https://TatoNoir.github.io/massivetrout/",
   trailingSlash: "ignore",
   build: {
     inlineStylesheets: "auto",
