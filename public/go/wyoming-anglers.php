49330e3d33a767d03bea4e207aa832a053320aed
Mariano <marianopainefil@gmail.com>
2026-09-24 19:17:11 -0300

    modificacion wyoming anglers

--- public/go/wyoming-anglers.php
+++ public/go/wyoming-anglers.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Referral redirect: Cowboys Drifters - DESHABILITADO
+ * Referral redirect: Wyoming Anglers - DESHABILITADO
  * Antes enviaba mail de aviso con mtff_send. Ahora solo redirige,
  * porque el flujo actual exige suscripción vía CowboyModal antes de salir.
  * No enviar más mails de "Referral click".
@@ -8,7 +8,7 @@
 
 declare(strict_types=1);
 
-$target = 'https://wyocowboydrifters.com/';
+$target = 'https://wyominganglers.com/';
 
 header('Location: ' . $target, true, 302);
 exit;
